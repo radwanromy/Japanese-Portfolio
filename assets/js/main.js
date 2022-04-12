e97cbbc2289c2fa42df31355eca96cc0b75d53ef
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-04-12 07:18:35 +0600

    01JS commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+function scrollHeader() {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
@@ -7,7 +13,7 @@
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
-/* Link active work */ 
+/* Link active work */
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
@@ -16,7 +22,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== LIGHT DARK THEME ===============*/ 
+/*=============== LIGHT DARK THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
